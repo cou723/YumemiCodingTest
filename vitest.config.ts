799ba67af20b1ef80c78a9aef6a692cccb177e6d
cou <courange.c@gmail.com>
2024-02-20 14:36:43 +0900

    change vitest reporter actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
     },
   },
 });
