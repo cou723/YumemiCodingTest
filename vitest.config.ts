303f58aded1577e66504eb9048b85623cbbd00b0
cou <courange.c@gmail.com>
2024-02-20 12:23:41 +0900

    add @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
