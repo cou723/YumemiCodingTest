d9f04dd1f323a5848a8224dddbec5dcb0c32b6f0
cou <courange.c@gmail.com>
2024-03-04 04:02:57 +0900

    fix add role

--- src/components/PrefectureCheckboxes/CircularProgress.tsx
+++ src/components/PrefectureCheckboxes/CircularProgress.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./CircularProgress.module.css";
 const circularProgress = () => {
   // https://css-loaders.com/
-  return <div className={styles.circle} />;
+  return <div className={styles.circle} role="progressbar" />;
 };
 
 export default circularProgress;
